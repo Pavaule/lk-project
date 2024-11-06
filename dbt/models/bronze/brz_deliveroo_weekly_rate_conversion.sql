507eeb28496a79adbd154975595e61223b572ebd
paul <maillefer.paul@gmail.com>
2024-11-06 17:50:07 +0000

    Premier modèle dbt finito

--- dbt/models/bronze/brz_deliveroo_weekly_rate_conversion.sql
+++ dbt/models/bronze/brz_deliveroo_weekly_rate_conversion.sql
@@ -2,5 +2,5 @@ SELECT
   restaurant_id,
   starting_date,
   ending_date,
-  conversion_rate
-FROM {{ source('deliveroo','deliveroo_weekly_conversion_rate_and_customer_retention')}}
+  conversion_rate_order_count_divide_by_menu_view_count
+FROM {{ source('brouillon', 'deliveroo_weekly_conversion_rate_and_customer_retention')}}
